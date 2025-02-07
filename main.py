0338b2b52190f34a28635fa5c342edd6338a2c6c
Pedro Pinheiro <49202702+ppinheirosiqueira@users.noreply.github.com>
2025-02-06 22:36:11 -0300

    up dos bancos franceses

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import testar_banco
 # Tipo do campeonato - 0 para mata mata, 1 para campeonatos de pontos corridos
 #test.extrair_campeonato('https://www.sofascore.com/pt/torneio/futebol/brazil/brasileirao-serie-a/325#id:58766', "Campeonato Brasileiro", 2024, 1)
 
-# test.extrair_campeonato("https://www.sofascore.com/pt/torneio/futebol/europe/uefa-champions-league/7#id:52162","Liga dos Campeões", 2024, 2)
-test.extrair_dados_links("Liga dos Campeões", 2024)
+test.extrair_campeonato("https://www.sofascore.com/pt/torneio/futebol/france/ligue-1/34#id:28222","Ligue 1", 2021, 1)
+test.extrair_dados_links("Ligue 1", 2021)
 
-# testar_banco.verificar_jogos_faltantes('bancos/Ligue 1 2023.db', id_campeonato=1, id_inicial=1, id_final=20)
+# testar_banco.verificar_jogos_faltantes('bancos/Ligue 1 2022.db', id_campeonato=1, id_inicial=2, id_final=23)
